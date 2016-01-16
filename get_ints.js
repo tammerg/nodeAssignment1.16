c8964cead27e7570273850a2b9ce08fd76f877c1
tammerg <tg385@scarletmail.rutgers.edu>
2016-01-16 11:07:58 -0500

    still working on getting it to incrementally ignore an argument while still summing them up

--- get_ints.js
+++ get_ints.js
@@ -1,8 +1,15 @@
-var arg = process.argv;
+var arg = process.argv.slice(2);
+var array = [];
 var sum = 0;
 
-for(i = 2; i < process.argv.length; i++){
-  sum += parseFloat(process.argv[i]);
+for(i = 0; i < arg.length; i++){
+  sum = 0;
+  for(j = 0; i < arg.length; j++){
+    if (i!==j) {
+      sum +=parseFloat(arg[j])
+    }
+    array[i]=sum;
+  }
 }
-console.log(sum);
+console.log(array);
 
