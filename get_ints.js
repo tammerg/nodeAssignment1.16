6405df7530a08cdcd40f860aad93f628ef98b3f9
tammerg <tg385@scarletmail.rutgers.edu>
2016-01-16 10:38:19 -0500

    got initial node app adding up arguments, beginning to work on excluding arguments

---
+++ get_ints.js
@@ -0,0 +1,8 @@
+var arg = process.argv;
+var sum = 0;
+
+for(i = 2; i < process.argv.length; i++){
+  sum += parseFloat(process.argv[i]);
+}
+console.log(sum);
+
---
+++ get_sums_of_all_ints_except_at_index.js
@@ -0,0 +1,6 @@
+var arg = process.argv
+var sum = 0
+
+for(i = 0, i < process.argv.length, i++)
+  sum += parseFloat(process.argv[i]);
+};
